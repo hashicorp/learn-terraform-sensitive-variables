89290f2222ec49f430bdfab60e1a23abb9bc9e30
chujun1973 <145720024+chujun1973@users.noreply.github.com>
2023-11-19 20:28:07 -0800

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,4 +6,5 @@
 output "db_connect_string" {
   description = "MySQL database connection string"
   value       = "Server=${aws_db_instance.database.address}; Database=ExampleDB; Uid=${var.db_username}; Pwd=${var.db_password}"
+  sensitive   = true
 }
